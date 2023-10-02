c6f50a8cfcda3105e0ae51d7e74cc7612e683e5d
Gabriel Lopes <gabriellopes1005@gmail.com>
2023-10-02 20:22:05 -0300

    Projeto POO > Windows form > init

--- CRUD-Boletim/Form1.Designer.cs
+++ CRUD-Boletim/Form1.Designer.cs
@@ -28,13 +28,233 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.lblRa = new System.Windows.Forms.Label();
+            this.lblNome = new System.Windows.Forms.Label();
+            this.lblDisciplina = new System.Windows.Forms.Label();
+            this.lblNota1 = new System.Windows.Forms.Label();
+            this.lblNota2 = new System.Windows.Forms.Label();
+            this.lblMedia = new System.Windows.Forms.Label();
+            this.lblSituacao = new System.Windows.Forms.Label();
+            this.txtRa = new System.Windows.Forms.TextBox();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.txtNota1 = new System.Windows.Forms.TextBox();
+            this.txtNota2 = new System.Windows.Forms.TextBox();
+            this.txtMedia = new System.Windows.Forms.TextBox();
+            this.txtSituacao = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.btnIncluir = new System.Windows.Forms.Button();
+            this.btnExcluir = new System.Windows.Forms.Button();
+            this.btnAlterar = new System.Windows.Forms.Button();
+            this.btnPesquisar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblRa
+            // 
+            this.lblRa.AutoSize = true;
+            this.lblRa.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblRa.Location = new System.Drawing.Point(12, 9);
+            this.lblRa.Name = "lblRa";
+            this.lblRa.Size = new System.Drawing.Size(38, 24);
+            this.lblRa.TabIndex = 0;
+            this.lblRa.Text = "RA";
+            this.lblRa.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // lblNome
+            // 
+            this.lblNome.AutoSize = true;
+            this.lblNome.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNome.Location = new System.Drawing.Point(12, 33);
+            this.lblNome.Name = "lblNome";
+            this.lblNome.Size = new System.Drawing.Size(64, 24);
+            this.lblNome.TabIndex = 2;
+            this.lblNome.Text = "Nome";
+            // 
+            // lblDisciplina
+            // 
+            this.lblDisciplina.AutoSize = true;
+            this.lblDisciplina.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDisciplina.Location = new System.Drawing.Point(12, 59);
+            this.lblDisciplina.Name = "lblDisciplina";
+            this.lblDisciplina.Size = new System.Drawing.Size(100, 24);
+            this.lblDisciplina.TabIndex = 4;
+            this.lblDisciplina.Text = "Disciplina";
+            // 
+            // lblNota1
+            // 
+            this.lblNota1.AutoSize = true;
+            this.lblNota1.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNota1.Location = new System.Drawing.Point(12, 86);
+            this.lblNota1.Name = "lblNota1";
+            this.lblNota1.Size = new System.Drawing.Size(72, 24);
+            this.lblNota1.TabIndex = 5;
+            this.lblNota1.Text = "Nota 1";
+            // 
+            // lblNota2
+            // 
+            this.lblNota2.AutoSize = true;
+            this.lblNota2.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNota2.Location = new System.Drawing.Point(12, 111);
+            this.lblNota2.Name = "lblNota2";
+            this.lblNota2.Size = new System.Drawing.Size(72, 24);
+            this.lblNota2.TabIndex = 6;
+            this.lblNota2.Text = "Nota 2";
+            // 
+            // lblMedia
+            // 
+            this.lblMedia.AutoSize = true;
+            this.lblMedia.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMedia.Location = new System.Drawing.Point(12, 137);
+            this.lblMedia.Name = "lblMedia";
+            this.lblMedia.Size = new System.Drawing.Size(67, 24);
+            this.lblMedia.TabIndex = 7;
+            this.lblMedia.Text = "Média";
+            // 
+            // lblSituacao
+            // 
+            this.lblSituacao.AutoSize = true;
+            this.lblSituacao.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSituacao.Location = new System.Drawing.Point(12, 163);
+            this.lblSituacao.Name = "lblSituacao";
+            this.lblSituacao.Size = new System.Drawing.Size(93, 24);
+            this.lblSituacao.TabIndex = 8;
+            this.lblSituacao.Text = "Situação";
+            // 
+            // txtRa
+            // 
+            this.txtRa.Location = new System.Drawing.Point(56, 11);
+            this.txtRa.Name = "txtRa";
+            this.txtRa.Size = new System.Drawing.Size(219, 20);
+            this.txtRa.TabIndex = 9;
+            // 
+            // txtNome
+            // 
+            this.txtNome.Location = new System.Drawing.Point(82, 35);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(193, 20);
+            this.txtNome.TabIndex = 10;
+            // 
+            // txtNota1
+            // 
+            this.txtNota1.Location = new System.Drawing.Point(90, 88);
+            this.txtNota1.Name = "txtNota1";
+            this.txtNota1.Size = new System.Drawing.Size(185, 20);
+            this.txtNota1.TabIndex = 11;
+            // 
+            // txtNota2
+            // 
+            this.txtNota2.Location = new System.Drawing.Point(90, 113);
+            this.txtNota2.Name = "txtNota2";
+            this.txtNota2.Size = new System.Drawing.Size(185, 20);
+            this.txtNota2.TabIndex = 12;
+            // 
+            // txtMedia
+            // 
+            this.txtMedia.Location = new System.Drawing.Point(90, 139);
+            this.txtMedia.Name = "txtMedia";
+            this.txtMedia.Size = new System.Drawing.Size(185, 20);
+            this.txtMedia.TabIndex = 13;
+            // 
+            // txtSituacao
+            // 
+            this.txtSituacao.Location = new System.Drawing.Point(111, 165);
+            this.txtSituacao.Name = "txtSituacao";
+            this.txtSituacao.Size = new System.Drawing.Size(164, 20);
+            this.txtSituacao.TabIndex = 14;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(118, 61);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(157, 21);
+            this.comboBox1.TabIndex = 16;
+            // 
+            // btnIncluir
+            // 
+            this.btnIncluir.Location = new System.Drawing.Point(16, 195);
+            this.btnIncluir.Name = "btnIncluir";
+            this.btnIncluir.Size = new System.Drawing.Size(60, 23);
+            this.btnIncluir.TabIndex = 17;
+            this.btnIncluir.Text = "Incluir";
+            this.btnIncluir.UseVisualStyleBackColor = true;
+            // 
+            // btnExcluir
+            // 
+            this.btnExcluir.Location = new System.Drawing.Point(82, 195);
+            this.btnExcluir.Name = "btnExcluir";
+            this.btnExcluir.Size = new System.Drawing.Size(60, 23);
+            this.btnExcluir.TabIndex = 18;
+            this.btnExcluir.Text = "Excluir";
+            this.btnExcluir.UseVisualStyleBackColor = true;
+            // 
+            // btnAlterar
+            // 
+            this.btnAlterar.Location = new System.Drawing.Point(148, 195);
+            this.btnAlterar.Name = "btnAlterar";
+            this.btnAlterar.Size = new System.Drawing.Size(60, 23);
+            this.btnAlterar.TabIndex = 19;
+            this.btnAlterar.Text = "Alterar";
+            this.btnAlterar.UseVisualStyleBackColor = true;
+            // 
+            // btnPesquisar
+            // 
+            this.btnPesquisar.Location = new System.Drawing.Point(214, 195);
+            this.btnPesquisar.Name = "btnPesquisar";
+            this.btnPesquisar.Size = new System.Drawing.Size(62, 23);
+            this.btnPesquisar.TabIndex = 20;
+            this.btnPesquisar.Text = "Pesquisar";
+            this.btnPesquisar.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(287, 230);
+            this.Controls.Add(this.btnPesquisar);
+            this.Controls.Add(this.btnAlterar);
+            this.Controls.Add(this.btnExcluir);
+            this.Controls.Add(this.btnIncluir);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.txtSituacao);
+            this.Controls.Add(this.txtMedia);
+            this.Controls.Add(this.txtNota2);
+            this.Controls.Add(this.txtNota1);
+            this.Controls.Add(this.txtNome);
+            this.Controls.Add(this.txtRa);
+            this.Controls.Add(this.lblSituacao);
+            this.Controls.Add(this.lblMedia);
+            this.Controls.Add(this.lblNota2);
+            this.Controls.Add(this.lblNota1);
+            this.Controls.Add(this.lblDisciplina);
+            this.Controls.Add(this.lblNome);
+            this.Controls.Add(this.lblRa);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblRa;
+        private System.Windows.Forms.Label lblNome;
+        private System.Windows.Forms.Label lblDisciplina;
+        private System.Windows.Forms.Label lblNota1;
+        private System.Windows.Forms.Label lblNota2;
+        private System.Windows.Forms.Label lblMedia;
+        private System.Windows.Forms.Label lblSituacao;
+        private System.Windows.Forms.TextBox txtRa;
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.TextBox txtNota1;
+        private System.Windows.Forms.TextBox txtNota2;
+        private System.Windows.Forms.TextBox txtMedia;
+        private System.Windows.Forms.TextBox txtSituacao;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button btnIncluir;
+        private System.Windows.Forms.Button btnExcluir;
+        private System.Windows.Forms.Button btnAlterar;
+        private System.Windows.Forms.Button btnPesquisar;
     }
 }
 
